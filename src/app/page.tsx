919b8b127428ed4b3807e56e091245c09756c468
Farelart <ffjdganlaky@gmail.com>
2025-02-09 18:28:52 +0100

    first version

---
+++ src/app/page.tsx
@@ -0,0 +1,235 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="layout">
+      <h1 className="text-center font-extrabold text-4xl py-10 md:py-8">Happy birthday babe</h1>
+      <section className='flex flex-wrap gap-8 md:max-h-[500px] justify-center'>
+        <article className=' flex gap-4 items-center'>
+              <iframe 
+                style={{ borderRadius: "12px" }}
+                src="https://open.spotify.com/embed/track/5KDOTN45fAYM9Wt3G49e29?utm_source=generator" 
+                width="70%" 
+                height="80"
+                frameBorder="0"
+                allowFullScreen
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="eager"
+            />          
+            <div className="flip-card w-[250px] max-h-[250px] aspect-square">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <Image
+                    className='object-cover'
+                    src="/png/hot.png"
+                    alt='hot'
+                    width={250}
+                    height={250}
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <p className="text-center font-normal text-black text-xs sm:text-base p-4">come and let ME eat YOUUU.😋gimme that ASS🍑😈</p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article className=' flex gap-4 items-center'>
+            <div className="flip-card w-[250px] max-h-[250px] aspect-square">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <Image
+                      className='object-fit'
+                      src="/png/professional.png"
+                      alt='professional'
+                      width={250}
+                      height={250}
+                    />
+                  </div>
+                  <div className="flip-card-back">
+                    <p className="text-center font-normal text-black text-xs sm:text-base p-4">my business woman😏</p>
+                  </div>
+                </div>
+              </div>
+          </article>
+          <article className=' flex flex-row-reverse gap-4 items-center'>
+              <iframe 
+                style={{ borderRadius: "12px" }}
+                src="https://open.spotify.com/embed/track/0yHConG49418wPs8UfAt44?utm_source=generator" 
+                width="70%" 
+                height="80" 
+                frameBorder="0" 
+                allowFullScreen 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="eager"
+            />          
+            <div className="flip-card w-[250px] max-h-[250px] aspect-square">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <Image
+                      className='object-fit'
+                      src="/png/us.png"
+                      alt='us'
+                      width={250}
+                      height={250}
+                    />
+                  </div>
+                  <div className="flip-card-back">
+                    <p className="text-center text-black text-xs sm:text-base p-4">look at us shawty. we are made for eachother❤️</p>
+                  </div>
+                </div>
+              </div>
+          </article>
+          <article className=' flex gap-4 items-center'>
+              <iframe 
+                style={{ borderRadius: "12px" }}
+                src="https://open.spotify.com/embed/track/0ARK753YaiJbpLUk7z5yIM?utm_source=generator" 
+                width="70%" 
+                height="80" 
+                frameBorder="0" 
+                allowFullScreen 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="eager"
+            />          
+            <div className="flip-card w-[250px] max-h-[250px] aspect-square">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <Image
+                      className='object-fit'
+                      src="/png/airplane.png"
+                      alt='airplane'
+                      width={250}
+                      height={250}
+                    />
+                  </div>
+                  <div className="flip-card-back">
+                    <p className="text-center text-black text-xs sm:text-base p-4">ma femme💍💓</p>
+                  </div>
+                </div>
+              </div>
+          </article>
+        
+          <article className='flex gap-4 items-center'>
+            <div className="flip-card floating-animation w-[212px] min-h-[280px] aspect-square">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <Image
+                    className='object-fit'
+                    src="/png/princess34.png"
+                    alt='princess'
+                    width={212}
+                    height={280}
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <p className="text-center font-normal text-black text-xs sm:text-base p-4">happy 23th birthday babe.🎊🎊 <br /> when things become hard, just come to this website to remind you of <b>how special you are</b> and that <b>you have someone who&apos;ll always be there to protect you</b></p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article className='flex flex-row-reverse gap-4 items-center'>
+              <iframe 
+                style={{ borderRadius: "12px" }}
+                src="https://open.spotify.com/embed/track/6nGeLlakfzlBcFdZXteDq7?utm_source=generator" 
+                width="70%" 
+                height="80" 
+                frameBorder="0" 
+                allowFullScreen 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="eager"
+            />          
+            <div className="flip-card w-[250px] max-h-[250px] aspect-square">
+              <div className="flip-card-inner">
+                <div className="flip-card-front">
+                  <Image
+                    className='object-fit'
+                    src="/png/with_adam.png"
+                    alt='with_adam'
+                    width={250}
+                    height={250}
+                  />
+                </div>
+                <div className="flip-card-back">
+                  <p className="text-center font-normal text-black text-xs sm:text-base p-4">i can&apos;t wait for you to be the mother🫄🏻🫄🏻 of our children👶🏽👶🏽</p>
+                </div>
+              </div>
+            </div>
+          </article> 
+          <article className=' flex gap-4 items-center'>
+              <iframe 
+                style={{ borderRadius: "12px" }}
+                src="https://open.spotify.com/embed/track/2SLwbpExuoBDZBpjfefCtV?utm_source=generator" 
+                width="70%" 
+                height="80" 
+                frameBorder="0" 
+                allowFullScreen 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="eager"
+            />          
+            <div className="flip-card w-[250px] max-h-[250px] aspect-square">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <Image
+                      className='object-fit'
+                      src="/png/cutie.png"
+                      alt='cutie'
+                      width={250}
+                      height={250}
+                    />
+                  </div>
+                  <div className="flip-card-back">
+                    <p className="text-center font-normal text-black text-xs sm:text-base p-4">mais putain Widad. Why are u so beautiful?😩😏</p>
+                  </div>
+                </div>
+              </div>
+          </article>
+          <article className=' flex gap-4 items-center'>        
+          <div className="flip-card w-[250px] max-h-[250px] aspect-square">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <Image
+                      className='object-fit'
+                      src="/png/yazaki.png"
+                      alt='yazaki'
+                      width={250}
+                      height={250}
+                    />
+                  </div>
+                  <div className="flip-card-back">
+                    <p className="text-center text-black text-xs sm:text-base p-4">everything gonna be alright.okay? <br />i promise🤞🏾</p>
+                  </div>
+                </div>
+              </div>
+          </article>
+          <article className='flex flex-row-reverse gap-4 items-center'>
+              <iframe 
+                style={{ borderRadius: "12px" }}
+                src="https://open.spotify.com/embed/track/1yJ5fyqBusR7Verj8JGsKV?utm_source=generator&theme=0" 
+                width="70%" 
+                height="80" 
+                frameBorder="0" 
+                allowFullScreen 
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                loading="eager"
+            />          
+            <div className="flip-card w-[250px] max-h-[250px] aspect-square">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <Image
+                      className='object-fit'
+                      src="/png/dubai.png"
+                      alt='dubai'
+                      width={250}
+                      height={250}
+                    />
+                  </div>
+                  <div className="flip-card-back">
+                    <p className="text-center font-normal text-black text-xs sm:text-base p-4">getting married soon and traveling✈️ together, EVERYWHERE</p>
+                  </div>
+                </div>
+              </div>
+          </article> 
+      </section>
+      <p className='py-10 text-center md:text-left font-bold text-lg'>Let&apos;s stay together for the next <span className='counter'></span> years.</p>
+    </main>
+  )
+}
